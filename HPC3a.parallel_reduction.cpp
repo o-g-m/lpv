bc02679e837bcf42c894de1d665557ae7a8fa05d
p <spriteofficial11@gmail.com>
2026-05-05 05:26:53 +0530

    Update HPC3a.parallel_reduction.cpp

--- HPC3a.parallel_reduction.cpp
+++ HPC3a.parallel_reduction.cpp
@@ -1,39 +1,44 @@
-#include <iostream>
-#include <omp.h>
-using namespace std;
+ #include<iostream>
+  #include<omp.h>
 
-int minval(int a[],int n){
-    int m=a[0];
-    #pragma omp parallel for reduction(min:m)
-    for(int i=0;i<n;i++)
-        if(a[i]<m) m=a[i];
-    return m;
-}
+  using namespace std;
+  int minval(int arr[], int n){
+    int minval = arr[0];
+    #pragma omp parallel for reduction(min : minval)
+      for(int i = 0; i < n; i++){
+        if(arr[i] < minval) minval = arr[i];
+      }
+    return minval;
+  }
 
-int maxval(int a[],int n){
-    int m=a[0];
-    #pragma omp parallel for reduction(max:m)
-    for(int i=0;i<n;i++)
-        if(a[i]>m) m=a[i];
-    return m;
-}
+  int maxval(int arr[], int n){
+    int maxval = arr[0];
+    #pragma omp parallel for reduction(max : maxval)
+      for(int i = 0; i < n; i++){
+        if(arr[i] > maxval) maxval = arr[i];
+      }
+    return maxval;
+  }
 
-int sum(int a[],int n){
-    int s=0;
-    #pragma omp parallel for reduction(+:s)
-    for(int i=0;i<n;i++)
-        s+=a[i];
-    return s;
-}
+  int sum(int arr[], int n){
+    int sum = 0;
+    #pragma omp parallel for reduction(+ : sum)
+      for(int i = 0; i < n; i++){
+        sum += arr[i];
+      }
+    return sum;
+  }
 
-double avg(int a[],int n){
-    return (double)sum(a,n)/n;
-}
+  double average(int arr[], int n){
+    return (double)sum(arr, n) / n;
+  }
 
-int main(){
-    int a[]={1,2,3,4,5},n=5;
-    cout<<"The minimum value is: "<<minval(a,n)<<'\n';
-    cout<<"The maximum value is: "<<maxval(a,n)<<'\n';
-    cout<<"The summation is: "<<sum(a,n)<<'\n';
-    cout<<"The average is: "<<avg(a,n)<<'\n';
-}
+  int main(){
+    int n = 5;
+    int arr[] = {1,2,3,4,5};
+    cout << "The minimum value is: " << minval(arr, n) << '\n';
+    cout << "The maximum value is: " << maxval(arr, n) << '\n';
+    cout << "The summation is: " << sum(arr, n) << '\n';
+    cout << "The average is: " << average(arr, n) << '\n';
+    return 0;
+  }
